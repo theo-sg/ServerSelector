2f93bbd7607ccf1cb80e1fca00ce1f27fe3903a1
fraktal-x <theosg94@gmail.com>
2020-02-17 10:31:30 +0000

    Batch writer added

--- ServerSelector.py
+++ ServerSelector.py
@@ -1,14 +1,47 @@
-from keyboard import *
-from os import *
+import keyboard
+import os
 
 select = 1
 length = 4
+user = ""
+ip = ""
+servers = []
+
+class Server():
+    def __init__(self, name, version, description):
+        self.name = name
+        self.version = version
+        self.description = description
+
+
+def connect():
+    global user
+    global ip
+    data = []
+    f = open("connectdata.sese", "r")               #reads connect config file
+    data = [i.strip() for i in f]
+    user = (data[0].split('='))[1].strip()          #and assigns the variables
+    ip = (data[1].split('='))[1].strip()
+    f.close()
+
+def listServers():
+    global servers
+    f = open("serverdata.sese", "r")
+    # add functionality here
+    f.close()
+
+def writeModifiedBatch(u, v, w):    #where u = username, v = ip and w = name (assuming path == name)
+    f = open("Start Remote Server.bat", "w")
+    f.write("ssh -t {0}@{1} \"cd ~/Desktop/Minecraft/{2} && ./run.sh\"".format(u, v, w))
+    f.close()
+    
+		
 
 def menu():
-    system("cls")
+    os.system("cls")
     global select
     for i in range(1, 5):
-        print("{0}. {1} \t{2}".format(i, "option", "<<" if select == i else " "))
+        print("{0}. {1} \t\t{2}".format(i, "option", "<<<" if select == i else " "))
 
 def up():
     global select
@@ -27,12 +60,14 @@ def down():
     menu()
 
 def enter():
-	global select
-	print("{0} selected".format(select))
+    global select
+    print("{0} selected".format(select))
 
 menu()
-add_hotkey('up', up)
-add_hotkey('down', down)
-add_hotkey('enter', enter)
-wait()
+connect()
+writeModifiedBatch(user, ip, "TestServer")
+keyboard.add_hotkey('up', up)
+keyboard.add_hotkey('down', down)
+keyboard.add_hotkey('enter', enter)
+keyboard.wait()
 
